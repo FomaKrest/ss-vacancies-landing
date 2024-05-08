83733bbf9fbeb9bd5bc615450208d3bf9c84914f
FomaKrest <70705541+FomaKrest@users.noreply.github.com>
2024-05-08 15:34:01 +0500

    Adaptive and style corrections

--- src/carousel.js
+++ src/carousel.js
@@ -9,16 +9,31 @@ const images = document.querySelectorAll(".carousel-image");
 
 const totalImages = Object.keys(images).length;
 
+function changeColor() {
+  const lastCard = images[(currentIndex + 2) % images.length].querySelector(".carousel-card");
+  const currentCard = images[(prevIndex + 2) % images.length].querySelector(".carousel-card");
+
+  setTimeout(() => {
+    lastCard.classList.add("carousel-choosed-card");
+    currentCard.classList.add("carousel-not-choosed-card");
+  }, 10);
+
+  setTimeout(() => {
+    lastCard.classList.remove("carousel-not-choosed-card");
+    currentCard.classList.remove("carousel-choosed-card");
+  }, 490);
+}
+
 // Use this in your project, if you're doing it locally
 // const imageWidth = images[1].getBoundingClientRect().x;
 
-const imageWidth = 370;
+
 console.log("getbounding1", images[1].getBoundingClientRect());
 
 leftArrow.addEventListener("click", () => {
   prevIndex = currentIndex;
   currentIndex = (currentIndex - 1 + totalImages) % totalImages;
-  carousel.style.transform = `translateX(-${imageWidth}px)`;
+  carousel.style.transform = `translateX(-${Math.max(images[0].offsetWidth, 370)}px)`;
   carousel.insertBefore(images[currentIndex], carousel.firstChild);
 
   setTimeout(() => {
@@ -29,6 +44,7 @@ leftArrow.addEventListener("click", () => {
   setTimeout(() => {
     carousel.classList.remove("sliding-transition");
   }, 490);
+  changeColor();
 });
 
 rightArrow.addEventListener("click", () => {
@@ -37,11 +53,12 @@ rightArrow.addEventListener("click", () => {
   prevIndex = currentIndex;
   currentIndex = (currentIndex + 1) % totalImages;
 
-  carousel.style.transform = `translateX(-${imageWidth}px)`;
+  carousel.style.transform = `translateX(-${Math.max(images[0].offsetWidth, 370)}px)`;
 
   setTimeout(() => {
     carousel.appendChild(images[prevIndex]);
     carousel.classList.remove("sliding-transition");
     carousel.style.transform = "";
   }, 500);
+  changeColor();
 });
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const vacancies = ["web-programmer", "sales", "smm"]
 
 for (vacancy of vacancies) {
   const popUp = document.querySelector(`.${vacancy}-popup`);
+  const popUpBody = popUp.querySelector(".pop-up-body")
   const open = document.querySelector(`.${vacancy}-open`);
   const close = document.querySelector(`.${vacancy}-close`);
 
@@ -13,7 +14,15 @@ for (vacancy of vacancies) {
   close.addEventListener("click", () => {
     popUp.classList.remove("active");
   })
+
+  popUp.addEventListener("click", (e) => {
+    const withinBoundaries = e.composedPath().includes(popUpBody);
+   
+    if ( ! withinBoundaries ) {
+      console.log("Clicked")
+      popUp.classList.remove("active");
+    }
+    
+  })
 }
-
-
 
